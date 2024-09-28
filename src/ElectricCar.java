98d7d0c520acc771e584e46a28ebbd390d1f6c0c
nzo2020 <nz2020@bs.amalnet.k12.il>
2024-09-28 18:18:42 +0300

    Ex18

---
+++ src/ElectricCar.java
@@ -0,0 +1,6 @@
+public class ElectricCar extends Car {
+    @Override
+    public void accelerate() {
+        System.out.println("ElectricCar is accelerating silently.");
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,14 @@
+public class Vehicle {
+
+    public void accelerate() {
+        System.out.println("The vehicle is accelerating");
+    }
+
+    public void stop() {
+        System.out.println("The vehicle has stopped");
+    }
+
+    public void start() {
+        System.out.println("The vehicle is starting");
+    }
+}
